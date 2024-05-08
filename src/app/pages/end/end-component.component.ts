e25089920824145c54bdeea7ced9e3deb4c856fd
FuentesCubells <alejandrodfco@gmail.com>
2024-05-08 19:28:18 +0200

    changed url

---
+++ src/app/pages/end/end-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-component.component.html',
+  styleUrl: './end-component.component.css'
+})
+export class EndComponentComponent {
+
+}
